8560b009848d4c1d42fcc175afb56c92ab648ed6
Ivan <work.gorobec@gmail.com>
2022-07-28 02:19:17 +0500

    выделит в отдельный файл константы

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+export const Navigation = {
+  Favorite: "favorite",
+  Storage: "storage",
+  Deal: "deal",
+};
+
+export const Sort = {
+  All: "Все",
+  DirecSale: "Разовая продажа",
+  Auction: "Аукцион",
+};
