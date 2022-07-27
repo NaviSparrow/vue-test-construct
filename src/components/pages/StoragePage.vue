c1f2510057f8879b9d8fa63b077834a9fd731a46
Ivan <work.gorobec@gmail.com>
2022-07-28 02:17:38 +0500

    выделит страницу "склад"

---
+++ src/components/pages/StoragePage.vue
@@ -0,0 +1,42 @@
+<template>
+  <AppHeader />
+  <main class="page-main">
+    <div class="page-main__wrapper">
+      <h1 class="visually-hidden">Каталог</h1>
+      <div class="page-main__header controls">
+        <AppSort />
+        <AppSearch />
+      </div>
+      <AppCatalog />
+    </div>
+  </main>
+  <footer class="page-footer">
+    <h2 class="page-footer__title">
+      Developed by:
+      <span> Gorobec Ivan</span>
+    </h2>
+  </footer>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+import AppSort from "@/components/AppSort";
+import AppSearch from "@/components/AppSearch";
+import AppCatalog from "@/components/AppCatalog";
+import { Navigation, Sort } from "@/utils/utils";
+
+export default {
+  components: { AppCatalog, AppSearch, AppSort, AppHeader },
+  data() {
+    return {
+      offersList: [],
+      isDataLoaded: false,
+      currentPage: Navigation.Storage,
+      currenSort: Sort.All,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
