1f1abd5def5669cefa6c17835b11c7d607f8d1d2
Ivan <work.gorobec@gmail.com>
2022-07-28 02:15:59 +0500

    выделит компонент шапки

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="page-header">
+    <div class="page-header__decor"></div>
+    <nav class="page-header__nav navigation">
+      <div class="navigation__wrapper">
+        <ul class="navigation__menu">
+          <li class="navigation__item">
+            <a href="#" class="navigation__link">
+              <svg
+                class="navigation__link-icon"
+                width="16"
+                height="14"
+                focusable="false"
+                role="img"
+              >
+                <use href="/images/svg/header-menu.svg#favorites"></use>
+              </svg>
+              <span class="navigation__label">Избранное</span>
+            </a>
+          </li>
+          <li class="navigation__item navigation__item--active">
+            <a href="#" class="navigation__link">
+              <svg
+                class="navigation__link-icon"
+                width="18"
+                height="20"
+                focusable="false"
+                role="img"
+              >
+                <use href="/images/svg/header-menu.svg#storage"></use>
+              </svg>
+              <span class="navigation__label">Склад</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="#" class="navigation__link">
+              <svg
+                class="navigation__link-icon"
+                width="18"
+                height="20"
+                focusable="false"
+                role="img"
+              >
+                <use href="/images/svg/header-menu.svg#deal"></use>
+              </svg>
+              <span class="navigation__label">Сделки</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped></style>
