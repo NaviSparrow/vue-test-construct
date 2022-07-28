97f953b0900dc40df010ff6065448047710de400
Ivan <work.gorobec@gmail.com>
2022-07-29 03:50:50 +0500

    добавит страницу со сделками

---
+++ src/components/pages/DealsPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <page-header />
+  <main class="page-main">
+    <div class="page-main__wrapper">
+      <h1 class="visually-hidden">Сделки</h1>
+      <div class="page-main__header controls">
+        <header-sort />
+        <!--        <header-search-->
+        <!--          :search-term="searchTerm"-->
+        <!--          @update:serach-term="setSearchTerm"-->
+        <!--        />-->
+      </div>
+      <page-catalog :offers="sortedPaidOffers" />
+    </div>
+  </main>
+  <footer class="page-footer">
+    <h2 class="page-footer__title">
+      Developed by:
+      <span> Gorobec Ivan</span>
+    </h2>
+  </footer>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import PageHeader from "@/components/PageHeader";
+import HeaderSort from "@/components/HeaderSort";
+import PageCatalog from "@/components/PageCatalog";
+
+export default {
+  components: { PageCatalog, HeaderSort, PageHeader },
+  computed: {
+    ...mapGetters({
+      sortedPaidOffers: "main/sortedPaidOffers",
+    }),
+  },
+};
+</script>
+
+<style scoped></style>
