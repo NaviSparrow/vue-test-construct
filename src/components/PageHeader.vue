3e705458e0acb98127e003d52c93b172c87f6aa2
Ivan <work.gorobec@gmail.com>
2022-07-29 03:51:31 +0500

    добавит маршрутизацию

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,7 +4,14 @@
     <nav class="page-header__nav navigation">
       <div class="navigation__wrapper">
         <ul class="navigation__menu">
-          <li class="navigation__item">
+          <li
+            :class="{
+              navigation__item: true,
+              'navigation__item--active': isFavorites,
+            }"
+            @click="() => this.setCurrentPage('favorite')"
+            @click.prevent="$router.push('/favorites')"
+          >
             <a href="#" class="navigation__link">
               <svg
                 class="navigation__link-icon"
@@ -18,7 +25,14 @@
               <span class="navigation__label">Избранное</span>
             </a>
           </li>
-          <li class="navigation__item navigation__item--active">
+          <li
+            :class="{
+              navigation__item: true,
+              'navigation__item--active': isStorage,
+            }"
+            @click="() => this.setCurrentPage('storage')"
+            @click.prevent="$router.push('/')"
+          >
             <a href="#" class="navigation__link">
               <svg
                 class="navigation__link-icon"
@@ -32,7 +46,14 @@
               <span class="navigation__label">Склад</span>
             </a>
           </li>
-          <li class="navigation__item">
+          <li
+            :class="{
+              navigation__item: true,
+              'navigation__item--active': isDeals,
+            }"
+            @click="() => this.setCurrentPage('deals')"
+            @click.prevent="$router.push('/deals')"
+          >
             <a href="#" class="navigation__link">
               <svg
                 class="navigation__link-icon"
@@ -53,8 +74,33 @@
 </template>
 
 <script>
+import { mapMutations, mapState } from "vuex";
+import { Navigation } from "@/utils/utils";
+
 export default {
   name: "PageHeader",
+  computed: {
+    ...mapState({
+      currentPage: (state) => state.main.currentPage,
+    }),
+    isFavorites() {
+      return this.currentPage === Navigation.Favorite;
+    },
+    isStorage() {
+      return this.currentPage === Navigation.Storage;
+    },
+    isDeals() {
+      return this.currentPage === Navigation.Deals;
+    },
+  },
+  methods: {
+    ...mapMutations({
+      setCurrentPage: "main/setCurrentPage",
+    }),
+  },
+  routeClickHandler(name) {
+    console.log(name);
+  },
 };
 </script>
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import StoragePage from "@/components/pages/StoragePage";
+import BusinessPage from "@/components/pages/DealsPage";
+import FavoritesPage from "@/components/pages/FavoritesPage";
 
 const routes = [
   {
     path: "/",
     component: StoragePage,
+  },
+  {
+    path: "/deals",
+    component: BusinessPage,
+  },
+  {
+    path: "/favorites",
+    component: FavoritesPage,
   },
 ];
 
