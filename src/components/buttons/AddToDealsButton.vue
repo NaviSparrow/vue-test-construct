afd58e848c773eef220e34abd96f732d847f144e
Ivan <work.gorobec@gmail.com>
2022-07-30 18:20:37 +0500

    создаст компоненты для кнопок

---
+++ src/components/buttons/AddToDealsButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    @click="$emit('dataChange', 'isDeal', !isDeal)"
+    class="offer__add-deal-button button"
+    type="button"
+  >
+    Добавить в сделки
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AddToDealsButton",
+  props: {
+    isDeal: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/buttons/PayButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    @click="$emit('dataChange', 'isPaid', !isPaid)"
+    :class="{
+      'offer__pay-button button': !isPaid,
+      'offer__pay-button button offer__pay-button--disabled': isPaid,
+    }"
+    type="button"
+  >
+    {{ isPaid ? "Оплачено" : "Оплатить" }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AddToDealsButton",
+  props: {
+    isPaid: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
