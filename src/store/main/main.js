7ccf6eb3cc5151a6c7a739758328e2a6c8baee0d
Ivan <work.gorobec@gmail.com>
2022-07-30 18:21:24 +0500

    добавит логику изменения состояния у карточек

--- src/store/main/main.js
+++ src/store/main/main.js
@@ -1,11 +1,13 @@
 import { Navigation, Sort } from "@/utils/utils";
 import fakejson from "../../../fake.json";
 
+// prettier-ignore
 export const Main = {
   state: () => ({
     offersList: [],
-    paidOffers: [],
+    dealOffers: [],
     favoriteOffers: [],
+    paidOffers: [],
     isDataLoaded: false,
     currentPage: Navigation.Storage,
     currentSort: Sort.All,
@@ -17,35 +19,77 @@ export const Main = {
         if (state.currentSort === Sort.All) {
           return offers;
         }
-        return offers
-          .slice()
-          .filter((offer) => offer.type === state.currentSort);
+        return offers.slice().filter((offer) => offer.type === state.currentSort);
       };
     },
     sortedAllOffers(state, getters) {
       return getters.getSortedOffers(state.offersList);
     },
-    sortedPaidOffers(state, getters) {
-      return getters.getSortedOffers(state.paidOffers);
+    sortedDealOffers(state, getters) {
+      return getters.getSortedOffers(state.dealOffers);
     },
     sortedFavoritesOffers(state, getters) {
       return getters.getSortedOffers(state.favoriteOffers);
     },
+    sortedPaidOffers(state, getters) {
+      return getters.getSortedOffers(state.paidOffers);
+    },
     sortedAndSearchedOffers(state, getters) {
-      return getters.sortedAllOffers.filter((offer) =>
-        offer.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-      );
+      return getters.sortedAllOffers
+          .filter((offer) => offer.title.toLowerCase().includes(state.searchTerm.toLowerCase()));
     },
   },
   mutations: {
     setOffers(state, payload) {
       state.offersList = payload;
     },
-    setPaidOffers(state, payload) {
-      state.paidOffers = [...state.paidOffers, payload];
+    setDealOffers(state, payload) {
+      state.dealOffers = payload;
     },
     setFavoriteOffers(state, payload) {
-      state.favoriteOffers = [...state.favoriteOffers, payload];
+      state.favoriteOffers = payload;
+    },
+    setPaidOffers(state, payload) {
+      state.paidOffers = payload;
+    },
+    toggleDealList(state, payload) {
+      const payloadData = { ...payload };
+      const isItemIncludes = state.dealOffers.some((offer) => offer.id === payloadData.id);
+      console.log(payloadData);
+
+      if (isItemIncludes) {
+        state.dealOffers = state.dealOffers.filter((offer) => offer.id !== payloadData.id);
+      } else {
+        state.dealOffers = [...state.dealOffers, payloadData];
+      }
+    },
+    toggleFavoriteList(state, payload) {
+      const payloadData = { ...payload };
+      console.log(payloadData);
+      const isItemIncludes = state.favoriteOffers.some((offer) => offer.id === payloadData.id);
+      console.log(isItemIncludes);
+
+      if (isItemIncludes) {
+        state.favoriteOffers = state.favoriteOffers.filter((offer) => offer.id !== payloadData.id);
+      } else {
+        state.favoriteOffers = [...state.favoriteOffers, payloadData];
+      }
+    },
+    togglePaidList(state, payload) {
+      const payloadData = { ...payload };
+      console.log(payloadData);
+      const isItemIncludes = state.paidOffers.some((offer) => offer.id === payloadData.id);
+
+      if (isItemIncludes) {
+        state.paidOffers = state.paidOffers.filter((offer) => offer.id !== payloadData.id);
+      } else {
+        state.paidOffers = [...state.paidOffers, payloadData];
+      }
+    },
+    changeOfferData(state, payload) {
+      console.log(payload)
+      const index = state.offersList.findIndex((offer) => offer.id === payload.id);
+      state.offersList[index] = {...state.offersList[index], [payload.prop]: payload.value};
     },
     setIsDataLoaded(state, payload) {
       state.isDataLoaded = payload;
@@ -64,8 +108,11 @@ export const Main = {
     fetchOffers({ commit }) {
       setTimeout(() => {
         commit("setOffers", [...fakejson.offers]);
+        commit("setDealOffers", fakejson.offers.filter((offer) => offer.isDeal));
+        commit("setFavoriteOffers", fakejson.offers.filter((offer) => offer.isFavorite));
+        commit("setPaidOffers", fakejson.offers.filter((offer) => offer.isPaid));
         commit("setIsDataLoaded", true);
-      }, 2000);
+      }, 1000);
     },
   },
   namespaced: true,
