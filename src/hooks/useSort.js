b52da056698640737ba99dac9c45455c33cef5ec
Ivan <work.gorobec@gmail.com>
2022-08-08 03:20:37 +0500

    выделит функционал сортировки в хук

---
+++ src/hooks/useSort.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { useStore } from "vuex";
+import { computed } from "vue";
+import { Sort } from "@/utils/utils";
+
+export default function useSort() {
+  const store = useStore();
+
+  const currentSort = computed(() => store.state.main.currentSort);
+  const setCurrentSort = (newSortValue) => store.commit("main/setCurrentSort", newSortValue);
+
+  const isCurrentSortAll = computed(() => currentSort.value === Sort.All);
+  const isCurrentSortAuction = computed(() => currentSort.value === Sort.Auction);
+  const isCurrentSortDirectSale = computed(() => currentSort.value === Sort.DirectSale);
+
+  return {
+    Sort,
+    setCurrentSort,
+    isCurrentSortAll,
+    isCurrentSortAuction,
+    isCurrentSortDirectSale,
+  };
+}
