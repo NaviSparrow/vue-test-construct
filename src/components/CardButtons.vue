51a87e738500aaa4b1eb5d46e3719953450309e8
Ivan <work.gorobec@gmail.com>
2022-08-09 02:26:39 +0500

    переведет компонент кнопок на script setup

--- src/components/CardButtons.vue
+++ src/components/CardButtons.vue
@@ -1,55 +1,49 @@
 <!--  eslint-disable-->
+<script setup>
+import useCardButtons from "@/hooks/useCardButtons";
+import PayButton from "@/components/buttons/PayButton";
+import DealButton from "@/components/buttons/DealButton";
+import FavoriteButton from "@/components/buttons/FavoriteButton";
+
+const props = defineProps({
+  cardId: Number,
+  isFavorite: Boolean,
+  isDeal: Boolean,
+  isPaid: Boolean,
+});
+
+const {
+  changeCardData,
+  isDealButtonVisible,
+  isFavoriteButtonVisible,
+  isPayButtonVisible,
+} = useCardButtons(props);
+</script>
+
 <template>
   <div class="offer__button-wrapper">
     <deal-button
       :is-deal="isDeal"
       v-if="isDealButtonVisible"
-      @click="() => changeCardData({ prop: 'isDeal', value: !isDeal, id: cardId })"
+      @click="
+        () => changeCardData({ prop: 'isDeal', value: !isDeal, id: cardId })
+      "
     />
     <pay-button
       v-if="isPayButtonVisible"
       :is-deal="isDeal"
       :is-paid="isPaid"
-      @click="() => changeCardData({ prop: 'isPaid', value: !isPaid, id: cardId })"
+      @click="
+        () => changeCardData({ prop: 'isPaid', value: !isPaid, id: cardId })
+      "
     />
     <favorite-button
       v-if="isFavoriteButtonVisible"
       :is-favorite="isFavorite"
-      @click="() => changeCardData({ prop: 'isFavorite', value: !isFavorite, id: cardId })"
+      @click="
+        () =>
+          changeCardData({ prop: 'isFavorite', value: !isFavorite, id: cardId })
+      "
     />
   </div>
 </template>
-
-<script>
-import PayButton from "@/components/buttons/PayButton";
-import useCardButtons from "@/hooks/useCardButtons";
-import DealButton from "@/components/buttons/DealButton";
-import FavoriteButton from "@/components/buttons/FavoriteButton";
-
-export default {
-  name: "CardButtons",
-  components: { FavoriteButton, DealButton, PayButton },
-  props: {
-    cardId: Number,
-    isFavorite: Boolean,
-    isDeal: Boolean,
-    isPaid: Boolean,
-  },
-  setup(props) {
-    const {
-      changeCardData,
-      isDealButtonVisible,
-      isFavoriteButtonVisible,
-      isPayButtonVisible,
-    } = useCardButtons(props);
-    return {
-      changeCardData,
-      isPayButtonVisible,
-      isFavoriteButtonVisible,
-      isDealButtonVisible,
-    };
-  },
-};
-</script>
-
-<style scoped></style>
