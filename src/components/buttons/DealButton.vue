71b88759789c0b57230aefd2882ca3cc7d76ad00
Ivan <work.gorobec@gmail.com>
2022-08-09 01:24:35 +0500

    переведет компоненты кнопок на script setup

--- src/components/buttons/DealButton.vue
+++ src/components/buttons/DealButton.vue
@@ -1,16 +1,11 @@
+<script setup>
+defineProps({
+  isDeal: Boolean,
+});
+</script>
+
 <template>
   <button class="offer__add-deal-button button" type="button">
     {{ isDeal ? "В сделках" : "Добавить в сделки" }}
   </button>
 </template>
-
-<script>
-export default {
-  name: "DealButton",
-  props: {
-    isDeal: Boolean,
-  },
-};
-</script>
-
-<style scoped></style>
