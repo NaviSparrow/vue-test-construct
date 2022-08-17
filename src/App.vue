20c4fa22b97198775f591e30778694300f1b9649
Ivan <work.gorobec@gmail.com>
2022-08-18 00:00:32 +0500

    добавит общую для всех страниц обертку page-layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { useMainStore } from "@/store/main";
 import { onMounted } from "vue";
+import PageLayout from "@/pages/PageLayout";
 
 const mainStore = useMainStore();
 onMounted(() => mainStore.fetchOffersData());
 </script>
 
 <template>
-  <router-view></router-view>
+  <page-layout>
+    <router-view></router-view>
+  </page-layout>
 </template>
