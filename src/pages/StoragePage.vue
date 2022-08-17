31d8fed9bfeb2870da2971f8059ae70ea7f6c179
Ivan <work.gorobec@gmail.com>
2022-08-17 23:59:09 +0500

    уберет обертку page-layout и loader

--- src/pages/StoragePage.vue
+++ src/pages/StoragePage.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 import PageCatalog from "@/components/PageCatalog.vue";
-import PageLayout from "@/pages/PageLayout.vue";
-import PageLoader from "@/components/PageLoader.vue";
 import { useMainStore } from "@/store/main";
 import { storeToRefs } from "pinia";
 
 const store = useMainStore();
-const { offersWithSortAndSearch, isDataLoaded } = storeToRefs(store);
+const { offersWithSortAndSearch } = storeToRefs(store);
 </script>
 
 <template>
-  <page-loader v-if="!isDataLoaded" />
-  <page-layout v-else>
-    <page-catalog :offers="offersWithSortAndSearch" />
-  </page-layout>
+  <page-catalog :offers="offersWithSortAndSearch" />
 </template>
