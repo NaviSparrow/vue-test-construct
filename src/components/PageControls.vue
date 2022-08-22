bab7a4ea12b31a7cf977a6f07556ce5148241365
Ivan <work.gorobec@gmail.com>
2022-08-23 01:50:33 +0500

    удалит условие для отрисовки "поиска"

--- src/components/PageControls.vue
+++ src/components/PageControls.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import HeaderSearch from "@/components/HeaderSearch.vue";
 import HeaderSort from "@/components/HeaderSort.vue";
-import useNavigation from "@/hooks/useNavigation";
-
-const { isStoragePage } = useNavigation();
 </script>
 
 <template>
   <div :class="$style.controls">
     <header-sort />
-    <header-search v-if="isStoragePage" />
+    <header-search />
   </div>
 </template>
 
