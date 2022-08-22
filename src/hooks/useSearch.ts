ed991ed62f75dccd47ec1af8abc65b8e62e85161
Ivan <work.gorobec@gmail.com>
2022-08-23 01:48:53 +0500

    добавит функционал сброса поиска

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,10 +1,18 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useMainStore } from "@/store/main";
 
 export default function useSearch() {
-  const searchQuery = ref<string>("");
+  const searchQuery = ref("");
   const store = useMainStore();
 
-  const setSearchTerm = (): void => store.changeSearchTerm(searchQuery.value);
+  const setSearchTerm = (): void => {
+    store.changeSearchTerm(searchQuery.value);
+  };
+
+  watch(searchQuery, () => {
+    if (!searchQuery.value.length) {
+      store.changeSearchTerm("");
+    }
+  });
   return { searchQuery, setSearchTerm };
 }
